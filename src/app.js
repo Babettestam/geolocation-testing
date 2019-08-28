f20b7cf25f5025deed6a55062cd84e1ea5bceb02
Babette <babettestam@gmail.com>
2019-08-28 12:13:02 +0200

    Enable cors

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api', restRouter);
 app.use(
   '/',
